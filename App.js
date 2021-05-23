d1f40fbed17857379c345691437dcb8768dd2665
Rob Yeomans <76974577+YeomeoR@users.noreply.github.com>
2021-05-23 10:43:55 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Homepage from './Homepage.component'
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Homepage />
+    </>
+  );
+}
+
+export default App;
---
+++ Homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './Homepage.styles.scss'
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Jackets</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Trainers</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MEN</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
